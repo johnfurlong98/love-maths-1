0ebb7ec75b4b95ecca69a4bf33aa9d58e8ba469a
john furlong <furlongjohn088@gmail.com>
2024-02-16 15:54:41 +0000

    added multiplaction code for game

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,13 +30,15 @@ function runGame(gameType) {
    let num1 = Math.floor(Math.random() * 25) + 1;
    let num2 = Math.floor(Math.random() * 25 ) + 1;
 
-  if (gameType === "addition") {
+   if (gameType === "addition") {
     displayAdditionQuestion(num1, num2);
-  } else {
-    alert(`unknown game type: ${gameType}`);
-    throw `unknown game type: ${gameType}. Aborting!`;
-  }
-
+} else if (gameType === "multiply") {
+    displayMultiplyQuestion(num1, num2);
+} else {
+    alert(`Unknown game type: ${gameType}`);
+    throw `Unknown game type: ${gameType}. Aborting!`;
+}
+  
 }
 
 /**
@@ -72,11 +74,13 @@ function calculateCorrectAnswer() {
     let operator = document.getElementById("operator").innerText;
 
     if (operator === "+") {
-        return [operand1 + operand2, "addition"];
-    } else {
-        alert(`Unimplemented operator ${operator}`);
-        throw `Unimplemented operator ${operator}. Aborting!`;
-    }
+      return [operand1 + operand2, "addition"];
+  } else if (operator === "x") {
+      return [operand1 * operand2, "multiply"];
+  } else {
+      alert(`Unimplemented operator ${operator}`);
+      throw `Unimplemented operator ${operator}. Aborting!`;
+  }
 
 }
 
@@ -112,7 +116,11 @@ function displaySubtractQuestion() {
 
 }
 
-function displayMultiplyQuestion() {
+function displayMultiplyQuestion(operand1, operand2) {
+
+  document.getElementById('operand1').textContent = operand1;
+  document.getElementById('operand2').textContent = operand2;
+  document.getElementById('operator').textContent = "x";
 
 }
 
